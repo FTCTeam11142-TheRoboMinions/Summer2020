44c549e9bdf69777a1917f8b23b6161a484bcc3d
Ranai Srivastav <ranaisrivastav@gmail.com>
2020-06-20 23:33:05 -0500

    Day 2 Code

---
+++ Sumer2020/StartPorg1.java
@@ -0,0 +1,35 @@
+public class StartPorg1
+{
+    //two forward slashes make a comment
+    //every line of code must end in a semi colon
+    
+    int a= 0;
+    double d = 24;
+    char c = 'g';
+    String s = "s";
+    boolean b = false;
+    
+    //declaration  [DATATYPE = TYPE OF THE VARIABLE] [IDENTIFIER = NAME OF THE VARIABLE] [SEMICOLON = THIS IS WHERE THIS LINE ENDS]
+    int i;
+    
+    //initialization  [DATATYPE] [NAME/IDENTIFIER] = [VALUE TO BE STORED] 
+    int integer = 0;
+   
+  
+    int Number = 5;
+   
+    double decimal = -3.14;
+    char character = 'g'; //characters are enclosed in single quotes
+    //many characters together make a String
+    String sentence = "This variable has the value of " ;
+    
+    
+    
+    
+    public static void main(String args[])
+    {
+        int i;
+        
+        System.out.println("This variable has the value of " + i);
+    }
+}
---
+++ Sumer2020/StartPorg1_2.java
@@ -0,0 +1,31 @@
+class StartPorg1_2
+{
+    //this is a comment. two backslashes make a comment
+    //every line in java must end with a semi colon
+    
+    double decimal = -4562.354;     
+    char character = '8';                   
+    String Sentence = "787987";
+    
+    //declaration 
+    // [DATATYPE = TYPE OF THE DATA] [IDENTIFIER = NAME OF THE VARIABLE] [SEMICOLON = COMMAND ENDS HERE];
+   int i;
+   
+   //initialization
+   //[datatype] [IDENTIFIER] = [VALUE TO BE STORED] [SEMICOLON]
+   int number = 54;
+   int Number = 24;
+   int numbeR = 42;
+
+   //Null Pointer exception
+   public static void main(String args[])
+   {
+       int integer = -657842;          //integer values are integers. postive numbers, neg numbers and zero
+       integer = 8;
+       
+   }
+   
+   
+    
+    
+}
