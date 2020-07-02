4cff3017b5c79502b5483ce150c938290c49be2f
Ranai Srivastav <ranaisrivastav@gmail.com>
2020-07-02 18:03:43 -0500

    Changes

--- Sumer2020/Challenges/challenge3.java
+++ Sumer2020/Challenges/challenge3.java
@@ -5,20 +5,20 @@ public class challenge3
     /**
      *  You are still the owner of the shop. You are still making the cashier app.
      *  
-     *  Level 1 (EZ) Loops
+     *  Level 1 (EZ Dubs)
      *      Write a program that will calculate the running total for the counter. Ask the user to enter product values (double) that
      *      you will then add up until you get the total. Assume you are making this app for the "5 items only" counter. The user will have exactly 5 items.
      *      
-     *  Level 2 (EZ) Loops
+     *  Level 2 (EZ Dubs)
      *      Write a Program (WAP) that will calculate the the running total for as many items as the user wants. The user will enter "-1" (Integer) when they are done
      *      
-     *  Level 3 (Kinda Hard) Organization and code structure
+     *  Level 3 (Kinda Hard)
      *      Combine the program you wrote for challenge 2 with Challenge 3 Level 2. 
      *      So you will have to ask the user for what day it is and then store the discount that will be applicable. 
      *      The discount for weekdays is 10 percent and the store is closed on Weekdays. This is the discount that will be applied then add your code 
      *      for the runnng total. This will calculate the total cost. The last step will be to output the discount multiplied by the price.
      *      
-     * Level 4 (God Mode) Research and implementation
+     * Level 4 (God Mode)
      *      Go online and look up how to terminate a JAVA program using code. Then apply it. If the user enters a value that is unnacceptable like showing
      *      up on a weekend, then terminate the program then. Else, keep it going.
      *      
