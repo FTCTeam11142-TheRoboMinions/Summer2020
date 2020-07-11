fb7971080ce322193dc981c2bccf0d2a4a691640
Ranai Srivastav <ranaisrivastav@gmail.com>
2020-07-11 14:29:33 -0500

    Challenge 5 solution class was made and other system files updated

--- Sumer2020/Challenges/challenge5.java
+++ Sumer2020/Challenges/challenge5.java
@@ -1,34 +1,38 @@
 package Challenges;
 
+/**
+ Each level in this case will be a different program
+
+ Level 1
+ Accept a String from the user and display each cahracter in the string on a different line. Use the String library functions we learned
+ and the appropriate loop. Use the String class documentation if you can't figure out how to.
+
+ Level 2
+ Make an AAT cheating program
+ Present a menu of 5 items and in the menu have these 5 things
+ 1. Degrees to Radians Converter: Multiply the input by pi and divide by 180
+ 2. Radians to Degrees Converter: Multiply the input by 180 and divide by pi
+ 3. Law of Cosines Calculator given 2 sides and an angle : 1st Input squared plus second input suared minus the cosine of the angle in radians
+ 4. Polar to Rectangular Converter : Use Sin and Cosine to calulate the x and y coordinates
+ 5. Rectangular to Polar Converter : Use the pythagorean identity to calculate radius and inverse tangent to calculate the angle
+
+ Level 3
+ One of the converters above has a dedicated function for it. If you can figure out which one from the JAVA documentation
+ for the Math class, you will reduce your work a lot.
+
+ Rules are the same as always
+ 0. Indent your code correctly
+ 1. Have variable names that make sense
+ 2. Follow one convention systematically. If your brackets start on the next line as mine do, Do not change that
+ 3. Try to make the code as concise as possible. Not the least line numbers but logically, as simple as it can be
+ 4. Make the program funny or sarcastic bc why not.
+
+ **/
+
 public class challenge5
 {
-    /**
-       Each level in this case will be a different program
-       
-       Level 1
-            Accept a String from the user and display each cahracter in the string on a different line. Use the String library functions we learned
-            and the appropriate loop. Use the String class documentation if you can't figure out how to. 
-            
-       Level 2
-            Make an AAT cheating program
-            Present a menu of 5 items and in the menu have these 5 things
-             1. Degrees to Radians Converter: Multiply the input by pi and divide by 180
-             2. Radians to Degrees Converter: Multiply the input by 180 and divide by pi 
-             3. Law of Cosines Calculator given 2 sides and an angle : 1st Input squared plus second input suared minus the cosine of the angle in radians
-             4. Polar to Rectangular Converter : Use Sin and Cosine to calulate the x and y coordinates
-             5. Rectangular to Polar Converter : Use the pythagorean identity to calculate radius and inverse tangent to calculate the angle
-             
-       Level 3
-            One of the converters above has a dedicated function for it. If you can figure out which one from the JAVA documentation
-            for the Math class, you will reduce your work a lot.
-            
-         Rules are the same as alway
-             0. Indent your code correctly
-             1. Have variable names that make sense
-             2. Follow one convention systematically. If your brackets start on the next line as mine do, Do not change that
-             3. Try to make the code as concise as possible. Not the least line numbers but logically, as simple as it can be
-             4. Make the program funny or sarcastic bc why not.
-             
-             
-       **/
+    public static void main(String args[])
+    {
+
+    }
 }
